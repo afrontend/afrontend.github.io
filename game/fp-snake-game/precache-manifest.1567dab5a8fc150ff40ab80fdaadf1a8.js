7cf55b85bee3e059d42f2fc80834ca0ae0ea1411
afrontend <frontend.wordpress@gmail.com>
2019-01-20 20:54:59 +0900

    add fp-snake-game

---
+++ game/fp-snake-game/precache-manifest.1567dab5a8fc150ff40ab80fdaadf1a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "303a5e8aeb148323fde8",
+    "url": "/game/fp-snake-game/static/js/runtime~main.303a5e8a.js"
+  },
+  {
+    "revision": "39e7a5033691258abb06",
+    "url": "/game/fp-snake-game/static/js/main.39e7a503.chunk.js"
+  },
+  {
+    "revision": "83212498c3bf96ba60d5",
+    "url": "/game/fp-snake-game/static/js/1.83212498.chunk.js"
+  },
+  {
+    "revision": "39e7a5033691258abb06",
+    "url": "/game/fp-snake-game/static/css/main.c144e248.chunk.css"
+  },
+  {
+    "revision": "c1f5f208b3d02e49ff2d034f37e04f2a",
+    "url": "/game/fp-snake-game/index.html"
+  }
+];
