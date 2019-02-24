18124b0b6099af92f83a2f45a4894377a7dcabaa
afrontend <frontend.wordpress@gmail.com>
2019-02-24 18:56:39 +0900

    update fp-maze-game

---
+++ game/fp-maze-game/precache-manifest.364e58b11c92296d709fec0b982e7086.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41cc5114203ab1c844c5",
+    "url": "/game/fp-maze-game/static/js/runtime~main.41cc5114.js"
+  },
+  {
+    "revision": "240fa45cd609d3775da7",
+    "url": "/game/fp-maze-game/static/js/main.240fa45c.chunk.js"
+  },
+  {
+    "revision": "afee82501fd515c5b05c",
+    "url": "/game/fp-maze-game/static/js/1.afee8250.chunk.js"
+  },
+  {
+    "revision": "240fa45cd609d3775da7",
+    "url": "/game/fp-maze-game/static/css/main.67a48c32.chunk.css"
+  },
+  {
+    "revision": "58f015e716cdaaa915200939ce605319",
+    "url": "/game/fp-maze-game/index.html"
+  }
+];
