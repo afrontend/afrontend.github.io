e0452658083794de704257a0d67fd7eda7838717
afrontend <frontend.wordpress@gmail.com>
2019-02-26 22:07:55 +0900

    update fp-maze-game

--- game/fp-maze-game/precache-manifest.a24f26e941ed3cde0a3b80867e95b9ad.js
+++ game/fp-maze-game/precache-manifest.a24f26e941ed3cde0a3b80867e95b9ad.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/game/fp-maze-game/static/js/runtime~main.41cc5114.js"
   },
   {
-    "revision": "f2e2e32141f837f8112d",
-    "url": "/game/fp-maze-game/static/js/main.f2e2e321.chunk.js"
+    "revision": "8aa87c0b07e4e2114289",
+    "url": "/game/fp-maze-game/static/js/main.8aa87c0b.chunk.js"
   },
   {
     "revision": "53c5b1b386f1d7d126df",
     "url": "/game/fp-maze-game/static/js/1.53c5b1b3.chunk.js"
   },
   {
-    "revision": "f2e2e32141f837f8112d",
+    "revision": "8aa87c0b07e4e2114289",
     "url": "/game/fp-maze-game/static/css/main.e48f99bd.chunk.css"
   },
   {
-    "revision": "d301059ab68e89585153dcc5a7b59244",
+    "revision": "80d121fca07471d4040d7b86502b56ff",
     "url": "/game/fp-maze-game/index.html"
   }
 ];
--- game/fp-maze-game/service-worker.js
+++ game/fp-maze-game/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/game/fp-maze-game/precache-manifest.de3a23800052e0acd9b28f152b2d3bca.js"
+  "/game/fp-maze-game/precache-manifest.a24f26e941ed3cde0a3b80867e95b9ad.js"
 );
 
 workbox.clientsClaim();
