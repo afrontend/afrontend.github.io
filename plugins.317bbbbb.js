4002b9b609dcc51406942db15c432dc107292660
afrontend <frontend.wordpress@gmail.com>
2019-11-03 09:55:36 +0900

    Update angle game

--- plugins.317bbbbb.js
+++ plugins.317bbbbb.js
@@ -164,7 +164,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42423" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41671" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
