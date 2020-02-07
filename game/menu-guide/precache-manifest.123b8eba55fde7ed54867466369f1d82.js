89380acbcd5b53bbf8a1093325df060ac2e7a68a
afrontend <autoscripts@gmail.com>
2020-02-07 21:39:47 +0900

    Update menu-guide

---
+++ game/menu-guide/precache-manifest.123b8eba55fde7ed54867466369f1d82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9630e800b78b4c0683949941daacced0",
+    "url": "/game/menu-guide/index.html"
+  },
+  {
+    "revision": "6971aa38b47f60d9b249",
+    "url": "/game/menu-guide/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b11d027c53ff186b590b",
+    "url": "/game/menu-guide/static/js/2.c68cfd1b.chunk.js"
+  },
+  {
+    "revision": "6971aa38b47f60d9b249",
+    "url": "/game/menu-guide/static/js/main.333b5953.chunk.js"
+  },
+  {
+    "revision": "9b577d9ac8a72a241c3e",
+    "url": "/game/menu-guide/static/js/runtime-main.c47f7308.js"
+  }
+]);
